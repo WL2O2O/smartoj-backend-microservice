ff2d7f3b03d73764d261350c1e48dc86b2d383ad
WLei224 <1149630057@qq.com>
2024-01-02 16:53:52 +0800

    move common module

---
+++ smartoj-backend-common/src/main/java/com/wl2o2o/smartojbackendcommon/common/DeleteRequest.java
@@ -0,0 +1,22 @@
+package com.wl2o2o.smartojbackendcommon.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 删除请求
+ *
+ * @author <a href="https://github.com/wl2o2o">程序员CSGUIDER</a>
+ * @from <a href="https://wl2o2o.github.io">CSGUIDER博客</a>
+ */
+@Data
+public class DeleteRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    private static final long serialVersionUID = 1L;
+}
